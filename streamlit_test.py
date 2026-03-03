4878bda22fb7f2d0b469003fc5f063a8dee98eae
Rads <dradon796@norquest.ca>
2026-03-03 22:47:25 +0000

    commit

--- streamlit_test.py
+++ streamlit_test.py
@@ -3,9 +3,19 @@ import streamlit as st
 
 # Testing Section
 
+st.title("SPICE Dashboard Prototype Version 0.01")
 
-st.write("Testing Writing Command")
+st.header("HEADER TEST 1")
+st.write("Testing Writing Command - DATA UNDER HEADER 1")
 
 value = st.slider("Choose a Number:", 0, 100, 50)
-
 st.write("You have Selected:", value)
+
+st.header("HEADER TEST 2")
+st.write("Testing Writing Command - DATA UNDER HEADER 2")
+
+box_sel = st.selectbox("Select Month:", ['Jan', 'Feb', 'Mar'])
+st.write("You have Selected:", box_sel)
+
+
+
