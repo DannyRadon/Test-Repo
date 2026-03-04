cc080b4b622ac9e502aa2690d2869a3ff84b3e97
Rads <dradon796@norquest.ca>
2026-03-04 00:15:37 +0000

    commit

---
+++ pages/eda.py
@@ -0,0 +1,3 @@
+# Test File for Side-Bar Navigation
+
+st.write("TESTING FROM EDA PAGE")
--- streamlit_test.py
+++ streamlit_test.py
@@ -1,15 +1,18 @@
 # Import Pool
 import streamlit as st
 
-# Testing Section
+# --------------- Testing Section ----------------------------
 
-st.title("SPICE Dashboard Prototype Version 0.01")
+st.title("SPICE Dashboard Prototype v0.01")
+st.metric("Total Yield", "12,430 kWh", "+5%")
 
 st.header("HEADER TEST 1")
 st.write("Testing Writing Command - DATA UNDER HEADER 1")
 
+
 value = st.slider("Choose a Number:", 0, 100, 50)
 st.write("You have Selected:", value)
+
 
 st.header("HEADER TEST 2")
 st.write("Testing Writing Command - DATA UNDER HEADER 2")
@@ -17,7 +20,12 @@ st.write("Testing Writing Command - DATA UNDER HEADER 2")
 box_sel = st.selectbox("Select Month:", ['Jan', 'Feb', 'Mar'])
 st.write("You have Selected:", box_sel)
 
+
 st.header("HEADER TEST 3")
 st.write("Testing Another Thing Again")
 
 mp_sel = st.multiselect("Select Year(s):", ['2020', '2021', '2022', '2023'])
+date = st.date_input("Select Date")
+
+if st.button("Press Here"):
+    st.write("BOOM!")
