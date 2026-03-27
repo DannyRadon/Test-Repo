19c17df569bdea0abb03d4bedf84ce93c8953449
Rads <dradon796@norquest.ca>
2026-03-27 05:25:26 -0600

    Update data_load.py

--- helpers/data_load.py
+++ helpers/data_load.py
@@ -14,11 +14,13 @@ def load_data():
     # This is just a placeholder for dev purposes, the true data link will probably be cloud
     file_visser = 'data/visser_data_imputed.csv'
     file_bissell = 'data/bissell_data_imputed.csv'
+    file_aeso = 'data/aeso_pool_genv2.csv'
 
    
     df_visser = pd.read_csv(file_visser)
     df_bissell = pd.read_csv(file_bissell)
+    df_aeso = pd.read_csv(file_aeso)
     
 
 
-    return df_visser, df_bissell
+    return df_visser, df_bissell, df_aeso
